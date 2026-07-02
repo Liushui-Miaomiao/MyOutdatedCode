0382c44b4e1dce78bc59f79f3be17925275ea9a6
Liushui <liushui_miaomiao@163.com>
2026-07-02 18:19:36 +0800

    [Add] 添加项目代码
    添加 Nightmare Trip 游戏的修改器

---
+++ VS2019/C#/Nt_Cheater/Nt_Cheater/FormMain.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Nt_Cheater {
+	public partial class FormMain : Form {
+
+		/// <summary>
+		/// 进程名
+		/// </summary>
+		private const string PROCESS_NAME = "Nightmare_trip";
+
+		/// <summary>
+		/// 游戏实例
+		/// </summary>
+		private readonly MyGame _myGame = new MyGame();
+
+		/// <summary>
+		/// 窗口主函数
+		/// </summary>
+		public FormMain() {
+			if (!MyUtils.IsAdministrator()) {
+				MessageBox.Show(
+					"没有以管理员身份启动，\n可能会造成修改进程失败！", 
+					"管理员权限", 
+					MessageBoxButtons.OK, 
+					MessageBoxIcon.Warning
+				);
+
+				Environment.Exit(0);
+				return;
+			}
+
+			InitializeComponent();
+
+			ListboxDreampower.SelectedIndex = ListboxDreampower.Items.Count - 1;
+			ListboxLife      .SelectedIndex = ListboxLife      .Items.Count - 1;
+			ListboxBoom      .SelectedIndex = ListboxBoom      .Items.Count - 1;
+
+			// 设置窗口图标为可执行文件图标
+			try {
+				Icon = Icon.ExtractAssociatedIcon(
+					System.Reflection.Assembly.GetExecutingAssembly().Location
+				);
+			}
+			catch (ArgumentException) { }
+		}
+
+		/// <summary>
+		/// 主窗体加载后函数
+		/// </summary>
+		/// <param name="sender">发送器</param>
+		/// <param name="e">事件参数</param>
+		private void Form1_Load(object sender, EventArgs e) {
+			ButtonRefindGame_Click(null, null);
+		}
+
+		/// <summary>
+		/// 按钮_重新寻找游戏
+		/// </summary>
+		/// <param name="sender">发送器</param>
+		/// <param name="e">事件参数</param>
+		private void ButtonRefindGame_Click(object sender, EventArgs e) {
+			MyUtils.FindProcess(
+				PROCESS_NAME, 
+				CallBackFindProcessSuccess, 
+				CallBackFail
+			);
+
+			if (_myGame == null) {
+				return;
+			}
+
+			if (_myGame.Quality == MyGame.QUALITY_SMOOTH) {
+				CheckboxSmooth.Checked = true;
+			}
+		}
+
+		/// <summary>
+		/// 查找进程成功回调
+		/// </summary>
+		private void CallBackFindProcessSuccess() {
+			MyUtils.GetPidByProcessName(
+				PROCESS_NAME, 
+				CallBackGetProcessPidSuccess, 
+				CallBackFail
+			);
+		}
+		/// <summary>
+		/// 查找进程Pid成功回调
+		/// </summary>
+		/// <param name="pid"></param>
+		private void CallBackGetProcessPidSuccess(int pid) {
+			_myGame.pid = pid;
+
+			MyUtils.GetProcessBaseAddr(
+				PROCESS_NAME, 
+				CallBackGetProcessBaseAddrSuccess, 
+				CallBackFail
+			);
+		}
+		/// <summary>
+		/// 读取进程基地址成功回调
+		/// </summary>
+		/// <param name="baseAddr"></param>
+		private void CallBackGetProcessBaseAddrSuccess(IntPtr baseAddr) {
+			_myGame.baseAddr = baseAddr;
+			_myGame.Init();
+
+			MyToolStripStatusLabel.Text = 
+				"进程Pid：" + _myGame.pid + "，" +
+				"当前难度：" + _myGame.Difficulty
+			;
+			MyTimer.Enabled = true;
+
+			MyUtils.SetAllControlEnable(Controls, true);
+		}
+		/// <summary>
+		/// 失败回调
+		/// </summary>
+		private void CallBackFail() {
+			MyToolStripStatusLabel.Text = "打开游戏失败：" + PROCESS_NAME;
+			MyTimer.Enabled = false;
+
+			MyUtils.SetAllControlEnable(Controls, false);
+		}
+
+
+
+		/// <summary>
+		/// 按钮_得分
+		/// </summary>
+		/// <param name="sender">发送器</param>
+		/// <param name="e">事件参数</param>
+		private void ButtonScore_Click(object sender, EventArgs e) {
+			if (!int.TryParse(TextboxScore.Text, out int score)) {
+				score = 0;
+			}
+
+			_myGame.WriteMemory(MyAddress.SCORE, score);
+		}
+		/// <summary>
+		/// 按钮_得分率
+		/// </summary>
+		/// <param name="sender">发送器</param>
+		/// <param name="e">事件参数</param>
+		private void ButtonScorerate_Click(object sender, EventArgs e) {
+			if (!int.TryParse(TextboxScorerate.Text, out int scoreRate)) {
+				scoreRate = 0;
+			}
+
+			// 需要除以10
+			scoreRate /= 10;
+
+			_myGame.WriteMemory(MyAddress.SCORERATE, scoreRate);
+		}
+		/// <summary>
+		/// 按钮_梦能量
+		/// </summary>
+		/// <param name="sender">发送器</param>
+		/// <param name="e">事件参数</param>
+		private void ButtonDreampower_Click(object sender, EventArgs e) {
+			if (!int.TryParse((string)ListboxDreampower.SelectedItem, out int dreamPower)) {
+				dreamPower = 0;
+			}
+
+			_myGame.WriteMemory(MyAddress.DREAMPOWER, dreamPower);
+		}
+		/// <summary>
+		/// 按钮_残机
+		/// </summary>
+		/// <param name="sender">发送器</param>
+		/// <param name="e">事件参数</param>
+		private void ButtonLife_Click(object sender, EventArgs e) {
+			if (!int.TryParse((string)ListboxLife.SelectedItem, out int life)) {
+				life = 1;
+			}
+
+			_myGame.WriteMemory(MyAddress.LIFE, life);
+		}
+		/// <summary>
+		/// 按钮_BOOM
+		/// </summary>
+		/// <param name="sender">发送器</param>
+		/// <param name="e">事件参数</param>
+		private void ButtonBoom_Click(object sender, EventArgs e) {
+			if (!int.TryParse((string)ListboxBoom.SelectedItem, out int boom)) {
+				boom = 1;
+			}
+
+			_myGame.WriteMemory(MyAddress.BOOM, boom);
+		}
+
+
+
+
+		/// <summary>
+		/// 复选框_不消耗梦能量
+		/// </summary>
+		/// <param name="sender">发送器</param>
+		/// <param name="e">事件参数</param>
+		private void CheckboxDreampower_CheckedChanged(object sender, EventArgs e) {
+			CheckBox self = (CheckBox)sender;
+
+			// 梦能量真实地址
+			int dreamPowerRealAddr = _myGame.GetRealAddress(MyAddress.DREAMPOWER);
+
+			// 源代码：C7 05 | ~~ ~~ ~~ ~~ | 00 00 | 00 00
+			// 逆序后：05 C7 | ~~ ~~ ~~ ~~ | 00 00 | 00 00
+			int[] lpBuffer = new int[] { 0x05C7, dreamPowerRealAddr, 0x0000, 0x0000 };
+			int[] nSize    = new int[] { 2     , 4                 , 2     , 2      };
+
+			if (self.Checked) {
+				// 目标代码：90 90 | 90 90 | 90 90 | 90 90 | 90 90
+				// 逆序目标：90 90 | 90 90 | 90 90 | 90 90 | 90 90
+				lpBuffer = new int[] { 0x9090, 0x9090, 0x9090, 0x9090, 0x9090 };
+				nSize    = new int[] { 2     , 2     , 2     , 2     , 2      };
+			}
+
+			_myGame.WriteMemoryArray(
+				MyAddress.NOUSE_DREAMPOWER,
+				lpBuffer,
+				nSize
+			);
+		}
+		/// <summary>
+		/// 复选框_不消耗残机
+		/// </summary>
+		/// <param name="sender">发送器</param>
+		/// <param name="e">事件参数</param>
+		private void CheckboxLife_CheckedChanged(object sender, EventArgs e) {
+			CheckBox self = (CheckBox)sender;
+
+			// 残机真实内存地址
+			int lifeRealAddr = _myGame.GetRealAddress(MyAddress.LIFE);
+
+			// 源代码：FF 0D | ~~ ~~ ~~ ~~ ~~
+			// 逆序后：0D FF | ~~ ~~ ~~ ~~ ~~
+			int[] lpBuffer = new int[] { 0x0DFF, lifeRealAddr };
+			int[] nSize    = new int[] { 2     , 4            };
+
+			if (self.Checked) {
+				// 目标代码：90 90 | 90 90 | 90 90
+				// 逆序目标：90 90 | 90 90 | 90 90
+				lpBuffer = new int[] { 0x9090, 0x9090, 0x9090 };
+				nSize    = new int[] { 2     , 2     , 2      };
+			}
+
+			_myGame.WriteMemoryArray(
+				MyAddress.NOUSE_LIFE,
+				lpBuffer,
+				nSize
+			);
+		}
+		/// <summary>
+		/// 复选框_不消耗BOOM
+		/// </summary>
+		/// <param name="sender">发送器</param>
+		/// <param name="e">事件参数</param>
+		private void CheckboxBoom_CheckedChanged(object sender, EventArgs e) {
+			CheckBox self = (CheckBox)sender;
+
+			int[] lpBuffer =
+				self.Checked ?
+					// 目标代码：90
+					new int[] { 0x90 } :
+					// 源代码  ：4E
+					new int[] { 0x4E }
+			;
+			
+			_myGame.WriteMemoryArray(
+				MyAddress.NOUSE_BOOM,
+				lpBuffer,
+				new int[] { 1 }
+			);
+		}
+
+
+
+
+		/// <summary>
+		/// 复选框_修改画质
+		/// </summary>
+		/// <param name="sender">发送器</param>
+		/// <param name="e">事件参数</param>
+		private void CheckboxSmooth_CheckedChanged(object sender, EventArgs e) {
+			CheckBox self = (CheckBox)sender;
+
+			_myGame.Quality = 
+				self.Checked ? 
+					MyGame.QUALITY_SMOOTH : 
+					MyGame.QUALITY_NO_SMOOTH
+			;
+		}
+		/// <summary>
+		///	定时器_重新寻找游戏
+		/// </summary>
+		/// <param name="sender">发送器</param>
+		/// <param name="e">事件参数</param>
+		private void MyTimer_Tick(object sender, EventArgs e) {
+			ButtonRefindGame_Click(null, null);
+		}
+	}
+}
---
+++ VS2019/C#/Nt_Cheater/Nt_Cheater/MyClass/MyProcess.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace Nt_Cheater {
+	/// <summary>
+	/// 进程操作类
+	/// </summary>
+	class MyProcess {
+		// DLL函数声明
+
+		[DllImportAttribute("kernel32.dll", EntryPoint = "ReadProcessMemory")]
+		private static extern bool _ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, IntPtr lpBuffer, int nSize, IntPtr lpNumberOfBytesRead);
+
+		[DllImportAttribute("kernel32.dll", EntryPoint = "WriteProcessMemory")]
+		private static extern bool _WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, int[] lpBuffer, int nSize, IntPtr lpNumberOfBytesWritten);
+
+		[DllImportAttribute("kernel32.dll", EntryPoint = "OpenProcess")]
+		private static extern IntPtr _OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+		/// <summary>
+		/// 判断指定进程是否存在
+		/// </summary>
+		/// <param name="name">进程名</param>
+		/// <returns>指定进程是否存在</returns>
+		public static bool IsExistsProcess(string name) {
+			Process[] processes = Process.GetProcessesByName(name);
+
+			return processes.Length > 0;
+		}
+
+		/// <summary>
+		/// 判断程序是否以管理员身份启动
+		/// </summary>
+		/// <returns>程序是否以管理员身份启动</returns>
+		public static bool IsAdministrator() {
+			WindowsIdentity  current = WindowsIdentity.GetCurrent();
+			WindowsPrincipal wp      = new WindowsPrincipal(current);
+
+			return wp.IsInRole(WindowsBuiltInRole.Administrator);
+		}
+
+		/// <summary>
+		/// 通过进程名称获取进程pid
+		/// </summary>
+		/// <param name="name">进程名</param>
+		/// <returns>指定名称进程的pid</returns>
+		public static int GetPidByProcessName(string name) {
+			int pid = 0;
+
+			if (!IsExistsProcess(name)) {
+				goto END;
+			}
+
+			Process[] processes = Process.GetProcessesByName(name);
+			foreach (Process process in processes) {
+				pid = process.Id;
+
+				if (pid > 0) {
+					goto END;
+				}
+			}
+
+		END:
+			return pid;
+		}
+
+		/// <summary>
+		/// 通过进程名称获取进程起始地址
+		/// </summary>
+		/// <param name="name">进程名</param>
+		/// <returns>指定名称进程的起始地址</returns>
+		public static IntPtr GetProcessBaseAddr(string name) {
+			IntPtr baseAddr = IntPtr.Zero;
+
+			if (!IsExistsProcess(name)) {
+				goto END;
+			}
+
+			Process[] processes = Process.GetProcessesByName(name);
+			foreach (Process process in processes) {
+				baseAddr = process.MainModule.BaseAddress;
+
+				if (baseAddr != IntPtr.Zero) {
+					goto END;
+				}
+			}
+
+		END:
+			return baseAddr;
+		}
+
+		/// <summary>
+		/// 打开进程
+		/// </summary>
+		/// <param name="dwDesiredAccess"></param>
+		/// <param name="bInheritHandle"></param>
+		/// <param name="dwProcessId"></param>
+		/// <returns>进程句柄</returns>
+		public static IntPtr OpenProcess(
+			int dwDesiredAccess, 
+			bool bInheritHandle, 
+			int dwProcessId
+		) {
+			try {
+				return _OpenProcess(
+					dwDesiredAccess, 
+					bInheritHandle, 
+					dwProcessId
+				);
+			}
+			catch (Exception) { }
+
+			return IntPtr.Zero;
+		}
+
+		/// <summary>
+		/// 读进程内存
+		/// </summary>
+		/// <param name="hProcess"></param>
+		/// <param name="lpBaseAddress"></param>
+		/// <param name="lpBuffer"></param>
+		/// <param name="nSize"></param>
+		/// <param name="lpNumberOfBytesRead"></param>
+		/// <returns></returns>
+		public static bool ReadProcessMemory(
+			IntPtr hProcess, IntPtr lpBaseAddress, 
+			IntPtr lpBuffer, int nSize, 
+			IntPtr lpNumberOfBytesRead
+		) {
+			try {
+				return _ReadProcessMemory(
+					hProcess, lpBaseAddress, 
+					lpBuffer, nSize, 
+					lpNumberOfBytesRead
+				);
+			}
+			catch (Exception) { }
+
+			return false;
+		}
+
+		/// <summary>
+		/// 写进程内存
+		/// </summary>
+		/// <param name="hProcess"></param>
+		/// <param name="lpBaseAddress"></param>
+		/// <param name="lpBuffer"></param>
+		/// <param name="nSize"></param>
+		/// <param name="lpNumberOfBytesWritten"></param>
+		/// <returns></returns>
+		public static bool WriteProcessMemory(
+			IntPtr hProcess, IntPtr lpBaseAddress, 
+			int[]  lpBuffer, int    nSize, 
+			IntPtr lpNumberOfBytesWritten
+		) {
+			try {
+				return _WriteProcessMemory(
+					hProcess, lpBaseAddress, 
+					lpBuffer, nSize, 
+					lpNumberOfBytesWritten
+				);
+			}
+			catch (Exception) { }
+
+			return false;
+		}
+	}
+}
